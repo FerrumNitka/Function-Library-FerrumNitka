c5e34a507c574b31ceb69d1bcc153f091b0c00ae
FerrumNitka <nikita14abc@gmail.com>
2025-08-19 12:33:55 +0700

    Update 1.cpp

--- cpp/1.cpp
+++ cpp/1.cpp
@@ -16,10 +16,12 @@ void close_variables()
 
 void print_info()
 {
-	std::cout << "Function Library FerrumNitka" << tab << in_parentheses(version, square_brackets) << std::endl;
+	std::cout << "Function Library FerrumNitka" << tab << "v" << version << std::endl;
+	std::cout << "Copyright (c) 2025 FerrumNitka" << std::endl;
 }
 
 void set_color(uc color_text, uc color_background)
 {
 	SetConsoleTextAttribute(handle, color_text + 16 * color_background);
+
 }
